376cb243fa10fb8b04bd779c5036aca0bacdb6c3
Igbinedion Osaro Glorious <osaroglorious7@gmail.com>
2026-06-23 16:40:45 -0700

    Add data sources for AWS resources in main.tf
    Define data sources for availability zones, AMI, caller identity, and region.

---
+++ terraform/modules/core/main.tf
@@ -0,0 +1,27 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
